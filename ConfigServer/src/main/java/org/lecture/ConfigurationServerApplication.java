60d0445c225d59e8de9ba7c20fc252fab793e8b2
ViktoriaVass <2210859014@fh-burgenland.at>
2023-11-23 03:38:40 +0100

    feat: #22 all modules starting properly, DB connection is still working

--- ConfigServer/src/main/java/org/lecture/ConfigurationServerApplication.java
+++ ConfigServer/src/main/java/org/lecture/ConfigurationServerApplication.java
@@ -6,10 +6,18 @@
 
 package org.lecture;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication; // Indicates that this class is a Spring Boot application, enabling various features.
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer; // Enables a config server that can communicate with other applications.
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @EnableConfigServer
 @SpringBootApplication
@@ -18,5 +26,18 @@ public class ConfigurationServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigurationServerApplication.class, args);
+    }
+}
+
+@RestController
+class ServiceInstanceRestController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
     }
 }
