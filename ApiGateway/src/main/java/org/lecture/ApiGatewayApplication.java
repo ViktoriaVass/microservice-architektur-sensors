00b64a46de299e4906f503495974949479c47cb0
Linda <2210859017@fh-burgenland.at>
2024-01-06 19:08:07 +0100

    fix: #39 fixed typo in documentation

--- ApiGateway/src/main/java/org/lecture/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/org/lecture/ApiGatewayApplication.java
@@ -1,7 +1,7 @@
 /**
  * The ApiGateway class serves as a centralized interface that aggregates, manages, and controls access to APIs within a distributed system.
- * The main method in this class starts the Spring Boot application, allowing the database service to run.
- * To run the database service, run ConfigurationApplication, ServiceRegistryApplication, ApiGatewayApplication and then this class.
+ * The main method in this class starts the Spring Boot application, allowing the ApiGateway to run.
+ * To run the ApiGateway, run ConfigurationApplication, ServiceRegistryApplication and then this class.
  */
 
 package org.lecture;
